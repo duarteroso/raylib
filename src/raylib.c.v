d10fcb0bbf85d8469b84fa406fce836ad77be8df
Duarte Roso <duarteroso@users.noreply.github.com>
2024-10-24 18:24:42 +0200

    Update raylib.c.v

--- src/raylib.c.v
+++ src/raylib.c.v
@@ -1711,13 +1711,13 @@ pub fn C.UnloadShader(shader Shader)
 // Screen-space-related functions
 
 // Get a ray trace from mouse position
-pub fn C.GetMouseRay(mousePosition Vector2, camera Camera, camera)
+pub fn C.GetMouseRay(mousePosition Vector2, camera Camera) Ray
 
 // Get camera transform matrix (view matrix)
-pub fn C.GetCameraMatrix(camera Camera)
+pub fn C.GetCameraMatrix(camera Camera) Matrix
 
 // Get camera 2d transform matrix
-pub fn C.GetCameraMatrix2D(camera Camera2D)
+pub fn C.GetCameraMatrix2D(camera Camera2D) Matrix
 
 // Get the screen space position for a 3d world space position
 pub fn C.GetWorldToScreen(position Vector3, camera Camera) Vector2
